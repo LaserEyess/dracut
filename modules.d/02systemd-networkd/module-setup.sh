dbecfb63efe69f6c9da0de33b15b1b99d8afcd05
LaserEyess <lasereyess@users.noreply.github.com>
2020-06-26 09:22:42 -0400

    02systemd-networkd: enable network generation
    This adds systemd-resolved as well as systemd-timesyncd hooks the
    networkd module, as well as systemd-network-generator, to fully
    support initramfs networking.

--- modules.d/02systemd-networkd/module-setup.sh
+++ modules.d/02systemd-networkd/module-setup.sh
@@ -26,32 +26,28 @@ install() {
     inst_multiple -o \
         $systemdutildir/systemd-networkd \
         $systemdutildir/systemd-networkd-wait-online \
+        $systemdutildir/systemd-network-generator \
         $systemdsystemunitdir/systemd-networkd-wait-online.service \
         $systemdsystemunitdir/systemd-networkd.service \
         $systemdsystemunitdir/systemd-networkd.socket \
+        $systemdsystemunitdir/systemd-network-generator.service \
         $systemdutildir/network/99-default.link \
-        networkctl ip
+        networkctl ip resolvectl \
+        $systemdutildir/systemd-timesyncd \
+        $systemdutildir/systemd-resolved \
+        $systemdutildir/systemd-resolve-host \
+        $systemdsystemunitdir/systemd-resolved.service \
+        $systemdsystemunitdir/systemd-timesyncd.service \
+        $systemdsystemunitdir/time-sync.target
 
-        #hostnamectl timedatectl
-        # $systemdutildir/systemd-timesyncd \
-        # $systemdutildir/systemd-timedated \
-        # $systemdutildir/systemd-hostnamed \
-        # $systemdutildir/systemd-resolvd \
-        # $systemdutildir/systemd-resolve-host \
-        # $systemdsystemunitdir/systemd-resolved.service \
-        # $systemdsystemunitdir/systemd-hostnamed.service \
-        # $systemdsystemunitdir/systemd-timesyncd.service \
-        # $systemdsystemunitdir/systemd-timedated.service \
-        # $systemdsystemunitdir/time-sync.target \
-        # /etc/systemd/resolved.conf \
-
-
-    # inst_dir /var/lib/systemd/clock
+    inst_dir /var/lib/systemd/clock
 
     grep '^systemd-network:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
     grep '^systemd-network:' $dracutsysrootdir/etc/group >> "$initdir/etc/group"
-    # grep '^systemd-timesync:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
-    # grep '^systemd-timesync:' $dracutsysrootdir/etc/group >> "$initdir/etc/group"
+    grep '^systemd-timesync:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
+    grep '^systemd-timesync:' $dracutsysrootdir/etc/group >> "$initdir/etc/group"
+    grep '^systemd-resolve:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
+    grep '^systemd-resolve:' $dracutsysrootdir/etc/passwd >> "$initdir/etc/group"
 
     _arch=${DRACUT_ARCH:-$(uname -m)}
     inst_libdir_file {"tls/$_arch/",tls/,"$_arch/",}"libnss_dns.so.*" \
@@ -59,11 +55,35 @@ install() {
                      {"tls/$_arch/",tls/,"$_arch/",}"libnss_myhostname.so.*" \
                      {"tls/$_arch/",tls/,"$_arch/",}"libnss_resolve.so.*"
 
+    if [[ $host_only ]]; then
+        inst_multiple -o \
+            /etc/systemd/timesyncd.conf \
+            /etc/systemd/timesyncd.conf.d/*.conf \
+            /etc/systemd/resolved.conf \
+            /etc/systemd/resolved.conf.d/*.conf \
+            /etc/systemd/networkd.conf \
+            /etc/systemd/networkd.conf.d/*.conf
+    fi
+
+    # DNSSEC still has issues and distros who use systemd-resolved almost
+    # all universially disable it by default, including Fedora, Arch, and Ubuntu
+    # See Fedora's motivation's here:
+    # https://fedoraproject.org/wiki/Changes/systemd-resolved#DNSSEC
+    mkdir -p "$initdir/etc/systemd"
+    {
+        echo "[Resolve]"
+        echo "DNSSEC=no"
+    } >> "$initdir/etc/systemd/resolved.conf"
+
+    ln -sf /run/systemd/resolve/stub-resolv.conf "$initdir"/etc/resolv.conf
+
     for i in \
         systemd-networkd-wait-online.service \
+        systemd-network-generator.service \
         systemd-networkd.service \
-        systemd-networkd.socket
-#       systemd-timesyncd.service
+        systemd-networkd.socket \
+        systemd-timesyncd.service \
+        systemd-resolved.service
     do
         systemctl -q --root "$initdir" enable "$i"
     done
